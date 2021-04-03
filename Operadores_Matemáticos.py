2b82ca0d85fc284dfcc12484782b268dcfee1952
Juan Mendoza <juan_mendoza_medina@hotmail.com>
2021-04-03 08:34:26 -0600

    Esta en la primera actualización de git

---
+++ Operadores_Matemáticos.py
@@ -0,0 +1,15 @@
+# comentario, guardar es ctrl + s
+# Para abrir una terminal oprime Ctrl + shift + ñ
+# Si quieres ejecutar el código en la terminal solo typea "python [Nombre del archivo.py] y da enter"
+# Aqui no hay punto y coma al final de cada instrucción
+
+
+A = input("Inserta el primer cantidad\n") #Cuando se inserta la cantidad toma el formato string
+B = input("Inserta el segundo cantidad\n")
+A = int(A) #Aqui lo convertimos a entero
+B = int(B)
+
+suma = A + B
+
+print("La suma de las cantidades es: \n", suma)
+
