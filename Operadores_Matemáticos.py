afdd3a3198901f48021b3a56db992046edbe65c1
Juan Mendoza <juan_mendoza_medina@hotmail.com>
2021-04-03 12:33:52 -0600

    se agrega una lista de comandos y se actualiza el archivo de operadores python

--- Operadores_Matemáticos.py
+++ Operadores_Matemáticos.py
@@ -16,7 +16,30 @@ resta = A - B
 print("La resta de las cantidades es: \n", resta)
 
 multiplicacion = A * B
-print("La resta de las cantidades es: \n", multiplicacion)
+print("La multiplicación de las cantidades es: \n", multiplicacion)
 
+division = A / B
+print("La división de las cantidades es: \n", division)
 
+flotante = float(division)
+print("División convertida en float: \n", flotante)
+
+cociente = A // B
+print("El cociente de las cantidades es: \n", cociente)
+
+residuo = A % B 
+print("El residuo de las cantidades es: \n", residuo)
+
+potencia = A ** B 
+print("La potencia de la primera cantidad elevada a la segunda cantidad es: \n", potencia)
+
+import math
+raiz_cuadrada = math.sqrt(A) 
+print("La raíz cuadrada de la primera cantidad es: \n", raiz_cuadrada)
+
+string = str(raiz_cuadrada)
+print("Raíz cuadrada convertida a string: \n", string)
+
+redondeo = round(raiz_cuadrada, 2)
+print("Redondeo de la raíz cuadrada a dos digitos: \n", redondeo)
 
